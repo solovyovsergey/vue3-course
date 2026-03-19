7b04a214d8baad29bef31dd8c4adf78e316baf40
ssolovev <soloovushka@ya.ru>
2026-03-19 20:04:01 +0300

    add fetching posts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import PostForm from './components/PostForm.vue'
 import type { Post, PostFormData } from './types'
 import PostList from './components/PostList.vue'
-import BaseButton from './components/UI/BaseButton.vue'
-import BaseDialog from './components/UI/BaseDialog.vue'
+import axios from 'axios'
 
-const baseIdx = ref(0)
-
-const posts = ref<Post[]>([
-  { id: baseIdx.value++, title: 'title1', body: 'body1' },
-  { id: baseIdx.value++, title: 'title2', body: 'body2' },
-  { id: baseIdx.value++, title: 'title3', body: 'body3' },
-])
+const posts = ref<Post[]>([])
 
 const isDialogVisible = ref(false)
+const isPostsLoading = ref(false)
 
 const createPost = (post: PostFormData) => {
   posts.value.push({ ...post, id: Date.now() })
@@ -23,6 +17,18 @@ const createPost = (post: PostFormData) => {
 const removePost = (post: Post) => {
   posts.value = posts.value.filter((p) => p.id !== post.id)
 }
+
+const fetchPosts = () => {
+  isPostsLoading.value = true
+
+  axios
+    .get<Post[]>('https://jsonplaceholder.typicode.com/posts', { params: { _limit: 10 } })
+    .then((response) => (posts.value = response.data))
+    .catch((e) => alert(e.message))
+    .finally(() => (isPostsLoading.value = false))
+}
+
+onMounted(fetchPosts)
 </script>
 
 <template>
@@ -32,7 +38,8 @@ const removePost = (post: Post) => {
     <BaseDialog v-model:show="isDialogVisible">
       <PostForm @create="createPost" />
     </BaseDialog>
-    <PostList :posts="posts" @removePost="removePost" />
+    <PostList v-if="!isPostsLoading" :posts="posts" @removePost="removePost" />
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 
