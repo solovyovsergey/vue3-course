e53223bc615f6d81f74f844e1476ac74b0d5d20f
ssolovev <soloovushka@ya.ru>
2026-03-20 12:18:19 +0300

    add PostList animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -12,12 +12,14 @@ const { posts } = defineProps<Props>()
 <template>
   <div v-if="!!posts.length">
     <h3>Список постов</h3>
-    <PostItem
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('removePost', post)"
-    />
+    <TransitionGroup name="posts">
+      <PostItem
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('removePost', post)"
+      />
+    </TransitionGroup>
   </div>
   <h2 v-else class="empty-msg">Список постов пуст</h2>
 </template>
@@ -25,5 +27,20 @@ const { posts } = defineProps<Props>()
 <style scoped>
 .empty-msg {
   color: red;
+}
+.posts-move,
+.posts-enter-active,
+.posts-leave-active {
+  transition: all 0.5s ease;
+}
+
+.posts-enter-from,
+.posts-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.posts-leave-active {
+  position: absolute;
 }
 </style>
